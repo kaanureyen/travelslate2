288127004ed30c82bbb0cb04be924fd479fd05bc
cengizkursat <cengiz.kursat.1996@gmail.com>
2019-11-22 15:23:31 +0300

    Geçmiş ve Favori ekranları güncellendi

--- lib/screens/FavoriteScreen.dart
+++ lib/screens/FavoriteScreen.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
+import '../consts.dart';
 
 class FavoriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('fav');
+    return Scaffold(
+      body: Container(
+        color:Colors.red,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(55.0),
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundColor: kButtonColor,
+                  child: Icon(
+                      Icons.favorite_border,
+                          size: 100,
+                          color: Colors.red,
+                  ),
+                ),
+              ),
+              Center(
+                child: Text("İstediğiniz çeviriyi favorilere",
+                  style: TextStyle(
+                  fontSize: 20,
+                  )
+
+                ),
+              ),
+              Center(
+                child: Text(
+                  "eklemek için çeviri sayfasındaki",
+                    style: TextStyle(
+                      fontSize:20,
+                    ),
+                ),
+              ),
+              Center(
+                child:Text(
+                  "kalp simgesine dokunun.", style: TextStyle(
+                fontSize: 20,
+                ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/HistoryScreen.dart
+++ lib/screens/HistoryScreen.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import '../consts.dart';
 
 class HistoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('history');
+    return Scaffold(
+      body:Container(
+        color: Colors.green,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(55.0),
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundColor: kButtonColor,
+                  child: Icon(
+                    Icons.history,
+                    size: 100,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Center(
+                child: Text("Henüz bir çeviri yapmadınız",
+                    style: TextStyle(
+                      fontSize: 20,
+                    )
+
+                ),
+              ),
+
+            ],
+        ),
+      ),
+      ),
+    );
   }
 }
